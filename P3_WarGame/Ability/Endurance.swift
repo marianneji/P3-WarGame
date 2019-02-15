7344527339197278525a360195dc1b49dfc689d5
marianneji <marianneji@gmail.com>
2019-02-15 18:04:02 +0100

    change the damage of endurance for 20

--- P3_WarGame/Ability/Endurance.swift
+++ P3_WarGame/Ability/Endurance.swift
@@ -9,6 +9,6 @@
 import Foundation
 class Endurance: Ability {
     init() {
-        super.init(abilityName: "endurance", damage: 10, damageReceived: -10, type: .endurance)
+        super.init(abilityName: "endurance", damage: 20, damageReceived: -10, type: .endurance)
     }
 }

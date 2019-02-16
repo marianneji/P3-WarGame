16f21ac1ef3517ec86331ff7cf8eb7f036a0345f
marianneji <marianneji@gmail.com>
2019-02-16 14:59:54 +0100

    CHANGE DAMAGE OF AGILITY

--- P3_WarGame/Ability/Agility.swift
+++ P3_WarGame/Ability/Agility.swift
@@ -9,6 +9,6 @@
 import Foundation
 class Agility: Ability {
     init() {
-        super.init(abilityName: "agility", damage: 0, damageReceived: -30, type: .agility)
+        super.init(abilityName: "agility", damage: 10, damageReceived: 30, type: .agility)
     }
 }

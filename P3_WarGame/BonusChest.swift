6f109181a402b618069417cde4ed4ac445ab268a
marianneji <marianneji@gmail.com>
2019-02-28 13:39:03 +0100

    add bonusChest() to his class

--- P3_WarGame/BonusChest.swift
+++ P3_WarGame/BonusChest.swift
@@ -9,5 +9,19 @@
 import Foundation
 class BonusChest {
     
+    func bonusChest(character: Character, round: Int) {
+        guard round == Int.random(in: 1...40) else {
+            
+            return
+        }
+        let newWeapon = character.changeWeapon(character: character)
+        character.weapon = newWeapon
+        print("""
+            ⚔️⚔️⚔️⚔️A chest with new weapons has appeared in front of you with a new weapon much stronger inside⚔️⚔️⚔️⚔️
+            Now \(character.characterName) the \(character.type) is equipped with the new weapon \(newWeapon.weaponName): ⚔️ \(newWeapon.damage)
+            ⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️
+            
+            """)
+    }
 }
 

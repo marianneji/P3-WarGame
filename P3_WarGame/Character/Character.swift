c60e86877c2bcbd13edd544e098f05c83afe9825
marianneji <marianneji@gmail.com>
2019-02-28 16:12:21 +0100

    clean code/add comments

--- P3_WarGame/Character/Character.swift
+++ P3_WarGame/Character/Character.swift
@@ -31,7 +31,6 @@ class Character: CustomStringConvertible { // variable description utilisée pou
     convenience init() {
         self.init(lifePoints: 0, maxLife: 0, characterName: "", weapon: Weapon(weaponName: "", damage: 0, type: .Axe), type: .warrior, ability: Endurance())
     }
-    //
     init(lifePoints: Int,maxLife: Int, characterName: String, weapon: Weapon, type: CharacterType, ability: Ability) {
         self.lifePoints = lifePoints
         self.maxLife = maxLife
@@ -40,18 +39,21 @@ class Character: CustomStringConvertible { // variable description utilisée pou
         self.type = type
         self.ability = ability
     }
-    
+    /// function for the character to attack the opponent team
     func attack(_ target: Character) {
         // cond to check that the ability does not give more life points
         if target.ability.damageReceived >= damage  {
             target.ability.damageReceived = damage
             // Infos of the attack if the cond above is true
-            print("\n\(target.characterName) the \(target.type) has the ability \(target.ability.abilityName), he absorbs the damages caused :"
-                + "\nThe attack has no effect, but he will not gain more ❤️. "
-                + "\n\(target.characterName) have \(target.lifePoints) ❤️."
-                + "\n")
+            print("""
+                
+                \(target.characterName) the \(target.type) has the ability \(target.ability.abilityName), he absorbs the damages caused :"
+                The attack has no effect, but he will not gain more ❤️.
+                \(target.characterName) have \(target.lifePoints) ❤️.
+                
+                """)
         } else {
-            // computed property to add the property, of the ability, damage received to the target
+            // computed property to add the ability damage received to the target
             var targetProtectionAbility: Int {
                 get {
                     return target.lifePoints + target.ability.damageReceived
@@ -60,43 +62,52 @@ class Character: CustomStringConvertible { // variable description utilisée pou
                     target.lifePoints = newValue
                 }
             }
+            // get only computed property to get the result of the attack for the print of the attack
+            var resultOfTheAttack: Int {
+                    return damage - target.ability.damageReceived
+            }
             // substraction of the computed properties to obtain the new value of target life
             targetProtectionAbility -= damage
+            // condition if the character has more damage to his life
             if target.lifePoints <= 0 {
                 print("""
-                    💀💀💀\(target.characterName) the \(target.type) is dead !💀💀💀
-                    His ability \(target.ability.abilityName) was not strong enough to protect him from the \(damage) damages caused !
+                                    💀💀💀\(target.characterName) the \(target.type) is dead !💀💀💀
+                    His ability \(target.ability.abilityName) was not strong enough to protect him from the \(resultOfTheAttack) damages caused !
                     
                     """)
                 target.lifePoints = 0
+                
             } else {
             // Infos of the attack
             print("""
-                \(target.characterName) the \(target.type) has lost \(damage) ♡ from:
+                
+                \(target.characterName) the \(target.type) has lost \(resultOfTheAttack) ❤️ from:
                 \(weapon.damage) of the \(weapon.weaponName) damages and \(ability.damage) of the \(ability.type) damages.
-                                          BUT.... Hahaha
+                                          BUT....
                 With his ability \(target.ability.abilityName), he has been protected of \(target.ability.damageReceived) pts
-                \(target.characterName) the \(target.type) has \(target.lifePoints) ♡ !
+                \(target.characterName) the \(target.type) have \(target.lifePoints) ❤️ !
                 
                 """)
             }
         }
     }
-    
+    /// function to heal character of the same team
     func heal(_ member: Character) {
+        // condition not to exceed the character maxLife
         if (member.lifePoints + damage) > member.maxLife {
             member.lifePoints = member.maxLife
-            //print("\(member.characterName) the \(member.type) has the maximum ❤️: \(member.lifePoints) pts\n")
-
         } else {
             member.lifePoints += damage
-            print("\(member.characterName) the \(member.type) has been restore his ❤️ with \(damage) points from :"
-                + "\n\(weapon.damage) of the mage scepter and \(ability.damage) of the \(ability.type)"
-                + "\n\(member.characterName) the \(member.type) has now \(member.lifePoints) ❤️\n")
+            print("""
+                
+                \(member.characterName) the \(member.type) has been restore his ❤️ with \(damage) points from :
+                \(weapon.damage) of the mage \(weapon.weaponName) and \(ability.damage) of the \(ability.type)
+                \(member.characterName) the \(member.type) has now \(member.lifePoints) ❤️
+                
+                """)
         }
-        
     }
-    
+    /// func to change weapon of the chosen character
     func changeWeapon(character: Character) -> Weapon {
         let newWeapons = [
             Weapon(weaponName: "Bow", damage: Int.random(in: 20...35), type: .Crossbow),

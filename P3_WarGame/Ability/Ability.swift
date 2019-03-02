867b486dedbe6eeb97820bdbe541ce926413f704
marianneji <marianneji@gmail.com>
2019-03-02 10:44:22 +0100

    remove the enum of the abilities... useless

--- P3_WarGame/Ability/Ability.swift
+++ P3_WarGame/Ability/Ability.swift
@@ -9,36 +9,22 @@
 import Foundation
 
 
-enum AbilityType: String {
-    case agility, endurance, strength
 
-    var type: String {// variable calculée 
-        switch self {
-        case .agility:
-            return "agility"
-        case .endurance:
-            return "endurance"
-        case .strength:
-            return "strength"
-        }
-    }
-}
 
 class Ability {
     
     var abilityName: String
     var damage: Int
     var damageReceived: Int
-    var type: AbilityType
     
-    init(abilityName: String, damage: Int, damageReceived: Int, type: AbilityType) {
+    
+    init(abilityName: String, damage: Int, damageReceived: Int) {
         self.abilityName = abilityName
         self.damage = damage
         self.damageReceived = damageReceived
-        self.type = type
     }
     convenience init() {
-        self.init(abilityName: "", damage: 0, damageReceived: 0, type: .agility)
+        self.init(abilityName: "", damage: 0, damageReceived: 0)
     }
 }
 

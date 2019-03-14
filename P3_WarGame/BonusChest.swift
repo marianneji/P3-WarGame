dfe32784cb816a32df9e51429f6473cdb77d6953
marianneji <marianneji@gmail.com>
2019-03-14 11:52:27 +0100

    reverse position of the functions for clarity

--- P3_WarGame/BonusChest.swift
+++ P3_WarGame/BonusChest.swift
@@ -7,7 +7,8 @@
 //
 
 import Foundation
-// class BonusChest contains called in the function fighter()
+/* class BonusChest contains an array of new weapons, the func bonusChest() and switchWeapon()
+ called in the function fighter()*/
 
 class BonusChest {
     // array of new weapons of each different weapon type
@@ -18,13 +19,32 @@ class BonusChest {
         Weapon(weaponName: "Longsword", damage: Int.random(in: 20...35), type: .Sword),
         Weapon(weaponName: "King Sword", damage: Int.random(in: 20...35), type: .Sword),
         Weapon(weaponName: "Sword of Fortune", damage: Int.random(in: 20...35), type: .Sword),
-        Weapon(weaponName: "Battle Axe", damage: Int.random(in: 20...35), type: .Axe),
-        Weapon(weaponName: "Red Axe", damage: Int.random(in: 20...35), type: .Axe),
-        Weapon(weaponName: "Axe of Fury", damage: Int.random(in: 20...35), type: .Axe),
+        Weapon(weaponName: "Battle Axe", damage: Int.random(in: 21...35), type: .Axe),
+        Weapon(weaponName: "Red Axe", damage: Int.random(in: 21...35), type: .Axe),
+        Weapon(weaponName: "Axe of Fury", damage: Int.random(in: 21...35), type: .Axe),
         Weapon(weaponName: "Magic Wand", damage: Int.random(in: 20...35), type: .Scepter),
         Weapon(weaponName: "Black Wand", damage: Int.random(in: 20...35), type: .Scepter),
         Weapon(weaponName: "Demon Scepter", damage: Int.random(in: 20...35), type: .Scepter),
         ]
+    
+    /// func to switch weapon of the chosen character
+    private func switchWeapon(character: Character) -> Weapon {
+        
+        // create a variable to shuffle the array of bonus weapons
+        var randomWeapon = bonusWeapons.randomElement()
+        
+        // condition to be sure that the character will be equipped with a weapon of his type and not the same of the previous one
+        while randomWeapon?.type != character.weapon.type || character.weapon.weaponName == randomWeapon?.weaponName {
+            randomWeapon = bonusWeapons.randomElement()
+            
+            if character.weapon.type == randomWeapon?.type && character.weapon.weaponName != randomWeapon?.weaponName {
+                character.weapon = randomWeapon!
+                return randomWeapon!
+            }
+        }
+        // return forced unwrap weapon because we are sure that there are weapons with the right type in the array
+        return randomWeapon!
+    }
     
     /// func to get a new weapon for the selected character
     public func bonusChest(character: Character, round: Int) {
@@ -38,31 +58,12 @@ class BonusChest {
         
         print("""
             
-            🎁🎁🎁🎁A chest has appeared in front of you with a new weapon much stronger inside🎁🎁🎁🎁
-                            Now \(character.characterName) the \(character.type) is equipped with the \(newWeapon.weaponName): ⚔️ \(newWeapon.damage)
+                        🎁🎁🎁🎁 A chest has appeared in front of you with a new weapon inside 🎁🎁🎁🎁
+                                Now \(character.characterName) the \(character.type) is equipped with the \(newWeapon.weaponName): ⚔️ \(newWeapon.damage)
             
                                             ⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️
             
             """)
-    }
-    
-    /// func to switch weapon of the chosen character
-    private func switchWeapon(character: Character) -> Weapon {
-        
-        // create a variable to shuffle the array of bonus weapons
-        var randomWeapon = bonusWeapons.randomElement()
-        
-        // condition to be sure that the character will be equipped with a weapon of his type and a different weapon of the same type
-        while randomWeapon?.type != character.weapon.type || character.weapon.weaponName == randomWeapon?.weaponName {
-            randomWeapon = bonusWeapons.randomElement()
-            
-            if character.weapon.type == randomWeapon?.type && character.weapon.weaponName != randomWeapon?.weaponName {
-                character.weapon = randomWeapon!
-                return randomWeapon!
-            }
-        }
-        // return forced unwrap weapon because we are sure that there are weapons with the right type in the array
-        return randomWeapon!
     }
 }
 
